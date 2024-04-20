09453d7e225d17e76b9c23c9b949ce92032c5da8
OGGymBro <143493367+OGGymBro@users.noreply.github.com>
2024-04-20 21:26:27 +0530

    Initial Commit

---
+++ UserNotification/UserNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserNotificationApp.swift
+//  UserNotification
+//
+//  Created by Joaquim Menezes on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct UserNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
